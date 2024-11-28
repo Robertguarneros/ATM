b23849c62d85bf8ae04ad23f4a62e92d76c90772
Roberto <roberto.guarneros11@gmail.com>
2024-11-28 17:44:39 +0100

    test

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
+
+
 
 
 st.title("ATM Project")
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
