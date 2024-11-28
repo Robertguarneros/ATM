74cb6428a0659de65209895244d7498218fc2b75
Roberto <roberto.guarneros11@gmail.com>
2024-11-28 17:54:46 +0100

    test

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import streamlit as st
 import pandas as pd
 import altair as alt
-import plotly.express as px
 
 
 st.set_page_config(
--- settings.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.header("Settings")
-st.write(f"Settings for the ATM Project")
