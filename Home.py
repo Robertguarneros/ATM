3f4b2a53ed105744953fa7100c8fb534af08442c
Roberto <roberto.guarneros11@gmail.com>
2024-12-06 13:17:44 +0100

    added about

--- Home.py
+++ Home.py
@@ -9,6 +9,24 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+# Page title
 st.title("Welcome to the ATM Project!")
-st.write("Use the sidebar to navigate to different sections!")
+# Enable Altair dark theme
 alt.themes.enable("dark")
+
+# Add markdown text for objectives
+st.markdown("""
+## Objectives
+
+- Show statistics about the separation between flights and whether or not the separation requirements are fulfilled:
+    - Radar
+    - Wake
+    - Letter of Agreement
+- Show statistics of the position and corrected altitude when the departure (from 24L) starts turning.
+- Show whether radial 234 is crossed when departing.
+- Show IAS of departures at 850, 1500, and 3500 ft for both runways.
+- Altitude corrected and IAS of traffic at threshold when departing at 24L and 06R.
+- Horizontal (stereographical) distance from departures to TMR-40 when departing from 24L.
+""")
+
+st.write("On the right sidebar, you can navigate to the different sections of the project to view the statistics and visualizations.")
---
+++ pages/About.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import altair as alt
+
+# Streamlit page configuration
+st.set_page_config(
+    page_title="ATM Project",
+    page_icon="assets/logo_eurocontrol.png",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# Page title
+st.markdown("""
+# About the ATM Project
+This project was developed for PGTA in QT-2024 by Group 8:
+- Roberto Guarneros
+- Angela Nuñez
+- David Gracia
+""")
+
